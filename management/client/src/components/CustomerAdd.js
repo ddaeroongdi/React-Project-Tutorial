80f05c7b32ffc70b29eba87e45fcadf83e7ef13c
JJE <wjdwodms0922@naver.com>
2021-01-11 19:06:36 +0900

    Add Material UI Design for Add/Delete

--- management/client/src/components/CustomerAdd.js
+++ management/client/src/components/CustomerAdd.js
@@ -42,7 +42,8 @@ class CustomerAdd extends React.Component{
             birthday:'',
             gender:'',
             job:'',
-            fileName:''
+            fileName:'',
+            open:false
         })
         
     }
@@ -116,22 +117,16 @@ class CustomerAdd extends React.Component{
                         <TextField label="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
                         <TextField label="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
                     </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleForSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
                 </Dialog>
             </div>
           
-            /*
-            <form onSubmit={this.handleForSubmit}> 
-                <h1>고객 추가</h1>
-                프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>              
-                이름 : <input type="text" name="userName" value={this.state.userName} onChange={this.handleValueChange}/><br/>
-                생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가하기</button>
-            </form>
-            */
+            
         );
     }
 }
 
-export default CustomerAdd;
+export default withStyles(styles) (CustomerAdd);

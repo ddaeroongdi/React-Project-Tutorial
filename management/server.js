80c608dd851fbfe03cc6183b70109a036800e925
JJE <wjdwodms0922@naver.com>
2021-01-11 17:29:57 +0900

    first commit

---
+++ management/server.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {extended:true }));
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+//database에 적어둔 mysql정보 받아서 넣어줌
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
+const multer =require('multer');
+const upload = multer({dest:'./upload'});
+
+app.get('/api/customers', (req, res) => {
+    connection.query(
+      "SELECT * FROM CUSTOMER WHERE isDeleted = 0",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    );
+});
+
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req, res) => {
+  let sql = 'INSERT INTO CUSTOMER VALUES(null, ?, ?, ?, ?, ?,now(), 0)';
+  let image = '/image/' + req.file.filename;
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+  let params = [image, name, birthday, gender, job];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
+
+app.delete('/api/customers/:id', (req, res) => {
+  let sql = 'update customer set isDeleted = 1 where id = ?';
+  let params = [req.params.id];
+  connection.query(sql, params,
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
+});
+app.listen(port, () => console.log(`Listening in port ${port}`));
